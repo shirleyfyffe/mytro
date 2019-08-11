1cc3fa9ba9d70d045bcf0594a081e54fe6df502b
shirleyfyffe <53680122+shirleyfyffe@users.noreply.github.com>
2019-08-11 08:58:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /mytrojan
 cat config1.txt > config.json
-echo -e "    \"local_port\": 1080," >> config.json
+echo -e "    \"local_port\": $PORT," >> config.json
 cat config2.txt >> config.json
 ./trojan
